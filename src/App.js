9b7192953c4810af40350ad18144f712ac8428ac
liav20 <liav20@gmail.com>
2022-08-27 21:03:54 +0300

    fixed server dependency started tests

--- src/App.js
+++ src/App.js
@@ -5,15 +5,17 @@ import ManageQuestion from "./component/manageQuestions/manageQuestion";
 import ShowQuestion from "./component/manageQuestions/showQuestion/showQuestion";
 import CreateQuestion from "./component/manageQuestions/createQeustion/createQestion";
 import EditQuestion from "./component/manageQuestions/createQeustion/createQestion";
+import ManageTest from "./component/manageTests/manageTest";
 import "./App.css";
 
 function App() {
   return (
     <header>
-      <Routes>
+      <Routes>     
         <Route path="/" element={<ChooseAccount />}></Route>
         <Route path="/:id" element={<MainMenu />}></Route>
         <Route path="/Question" element={<ManageQuestion />}></Route>
+        <Route path="/Tests" element={<ManageTest />}></Route>
         <Route path="/Question/:id" element={<ShowQuestion />}></Route>
         <Route path="/createQuestion" element={<CreateQuestion />}></Route>
         <Route path="/editQuestion/:id" element={<EditQuestion />}></Route>
--- src/component/manageTests/manageTest.js
+++ src/component/manageTests/manageTest.js
@@ -1,2 +1,68 @@
-const ManageTest = () => {};
+import ServiceTest from "../../service/serviceTest";
+import { useNavigate } from "react-router-dom";
+import { useState, useEffect, useRef } from "react";
+
+
+const ManageTest = () => {
+    const navigate = useNavigate();
+    let service = new ServiceTest();
+    const ref = useRef();
+    const [isLoading, setIsLoading] = useState(true);
+    const [tests, setTests] = useState([]);
+    const [query, setQuery] = useState("");
+
+    useEffect(() => {
+        service.get().then((tests) => {
+            setTests(tests);
+            setTimeout(() => {
+                setIsLoading(false);
+            }, 3000);
+            console.log(tests);
+        });
+    }, []);
+
+    if (isLoading) {
+        return <h3>is Loading....</h3>;
+    }
+
+    return (
+        <div>
+            <h1>Manage Tests</h1>
+            
+            <div>
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Test Text and Tags</th>
+              <th>Last Update</th>
+              <th>Language</th>
+              <th>Passing Grade</th>
+              <th></th>
+            </tr>
+          </thead>
+          {tests.map((item) => (
+            <tr key={item.id}>
+              <td>{item.id}</td>
+              <td>
+                {item.name}
+                <br />
+                {item.tag}
+              </td>
+              <td>{item.Date}</td>
+              <td>{item.language}</td>
+              <td>{item.passingGrade}</td>
+              <td>
+                <button>Show</button>
+                <button>Edit</button>
+                <button>Duplicate</button>
+                <button>Delete</button>
+              </td>
+            </tr>
+          ))}
+        </table>
+      </div>
+        </div>
+    )
+};
 export default ManageTest;
